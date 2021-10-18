671bd2e7a262019a2789d0a2f4859bad674952d9
pucki1988 <andi.schuster17@googlemail.com>
2021-10-18 17:30:12 +0200

    Update

---
+++ js/style.js
@@ -0,0 +1,51 @@
+window.onscroll = function() {setScroll()};
+function setScroll()
+{
+
+    
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) 
+    {
+        if(window.innerWidth<991)
+        {
+            setPaddingTop("section","90px")
+        }
+        else{
+            setPaddingTop("section","110px")
+        }
+        document.getElementsByTagName("body")[0].classList.add("small-menu");
+        document.getElementsByTagName("header")[0].classList.add("position-fixed");
+       
+    }
+    else
+    {
+        if(window.innerWidth<991)
+        {
+            setPaddingTop("section","0px")
+        }
+        else{
+            setPaddingTop("section","10px")
+        }
+        document.getElementsByTagName("body")[0].classList.remove("small-menu");
+        document.getElementsByTagName("header")[0].classList.remove("position-fixed");
+
+       
+    }
+}
+
+
+function setPaddingTop(selektor,pTop){
+    var selektoren=document.querySelectorAll(selektor);
+
+    for(i=0;i<selektoren.length;i++)
+    {
+        document.getElementById(selektoren[i].id).style.paddingTop=pTop;
+    }
+
+}
+
+function clearCollapse()
+{
+    var myCol=document.getElementById("navbarNav")
+    myCol.classList.remove("show");
+    document.getElementById("mobile-toggler-check").checked=false;
+}
